2e45b2e35cfb251f6d959f50e8349ca3612a7af9
tmiri@synetec.co.uk <tmiri@synetec.co.uk>
2018-05-31 16:45:42 +0100

    Remove folder

--- CityInfo.API/Models/CityWithoutPointsOfInterestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CityInfo.API.Models
-{
-    public class CityWithoutPointsOfInterestDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
